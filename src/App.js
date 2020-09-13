69b456f3efdafab74cd7ebc04bd9bc59136ac22e
Sultan Oloyede <sultanoloyede@hotmail.com>
2020-09-13 09:33:46 -0400

    Fourth commit less css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import TodoList from './components/TodoList';
 import styled from 'styled-components';
 
+const TodoApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  width: 520px;
+  min-height: 600px;
+  text-align: center;
+  margin: 128px auto;
+  border-radius: 10px;
+  padding-bottom: 32px;
+`
+
 function App() {
   return (
-    <div className='todo-app'>
+    <TodoApp>
       <TodoList />
-    </div>
+    </TodoApp>
   );
 }
 
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,6 +5,31 @@ const Form = styled.form `
   margin-bottom: 32px;
 `
 
+const Input = styled.input`
+  padding: 14px 32px 14px 16px;
+  border-radius: 4px 0 0 4px;
+  border: 2px solid #5d0cff;
+  outline: none;
+  width: 320px;
+  background: transparent;
+  color: #fff;
+`
+
+const Button = styled.button`
+  padding: 16px;
+  border: none;
+  border-radius: 0 4px 4px 0;
+  cursor: pointer;
+  outline: none;
+  background: linear-gradient(
+    90deg,
+    rgba(93, 12, 255, 1) 0%,
+    rgba(155, 0, 250, 1) 100%
+  );
+  color: #fff;
+  text-transform: capitalize;
+`
+
 const TodoInputEdit = styled.input`
   padding: 14px 32px 14px 16px;
   border-radius: 4px 0 0 4px;
@@ -70,17 +95,17 @@ function TodoForm(props) {
         </>
       ) : (
         <>
-          <input
+          <Input
             placeholder='Add a todo'
+            placeholderTextColor='red'
             value={input}
             onChange={handleChange}
             name='text'
-            className='todo-input'
             ref={inputRef}
           />
-          <button onClick={handleSubmit} className='todo-button'>
+          <Button onClick={handleSubmit}>
             Add todo
-          </button>
+          </Button>
         </>
       )}
     </Form>
